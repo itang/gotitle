298f681843b82ca8586fdb4f7866ffb72c81bac8
itang <live.tang@gmail.com>
2016-01-23 21:06:19 +0800

    cleanup

--- gotitle.go
+++ gotitle.go
@@ -9,7 +9,7 @@ import (
 const st = "<title>"
 const et = "</title>"
 
-func GetTitleFromUrl(url string) (title string, err error) {
+func GetTitleFromUrl(url string) (string, error) {
 	content, err := gotang.HttpGetAsString(url)
 	if err != nil {
 		return "", err
@@ -20,13 +20,8 @@ func GetTitleFromUrl(url string) (title string, err error) {
 		end := strings.Index(content, et)
 		if end > 0 {
 			start := pos + len(st)
-			return strings.TrimSpace(substr(content, start, end)), nil
+			return strings.TrimSpace(content[start:end]), nil
 		}
 	}
 	return "", nil
-}
-
-func substr(s string, start, end int) string {
-	runes := []rune(s[start:end])
-	return string(runes)
 }
