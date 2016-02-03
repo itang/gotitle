8a32e6172967e83d2e1237965e3af881a032c8f1
itang <live.tang@gmail.com>
2016-02-03 09:43:08 +0800

    cleanup

--- cmd/gotitle/main.go
+++ cmd/gotitle/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/itang/gotang"
+	gtime "github.com/itang/gotang/time"
 	"github.com/itang/gotitle"
 )
 
@@ -13,12 +15,25 @@ func main() {
 		fmt.Println("请指定url.")
 	}
 
-	for _, url := range os.Args[1:] {
-		fmt.Printf(">http get '%v'...\n", url)
+	gotang.Time(func() {
+		for _, url := range os.Args[1:] {
+			fmt.Printf(">http get '%v'...\n", url)
 
-		title, err := gotitle.GetTitleFromUrl(url)
-		gotang.AssertNoError(err, "http请求出错!")
+			title, err := gotitle.GetTitleFromUrl(url)
+			gotang.AssertNoError(err, "http请求出错!")
 
-		fmt.Printf("\t->>title: %v\n", title)
-	}
+			fmt.Printf("\t->>title: %v\n", title)
+
+			displayResult(url, title)
+		}
+	})
+}
+
+func displayResult(url, title string) {
+	fmt.Println()
+	tpl := `rs << Read.new "%s",
+    title: "%s",
+    created_at: "%s"`
+	fmt.Printf(tpl, url, title, gtime.FormatDefault(time.Now()))
+	fmt.Print("\n\n")
 }
